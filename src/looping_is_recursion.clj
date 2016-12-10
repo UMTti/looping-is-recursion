bc1740a71474b1be3f135cc6978a8e134da7193e
UMTti <toivanenpihla@gmail.com>
2016-12-10 15:15:18 +0200

    going to change computer

--- src/looping_is_recursion.clj
+++ src/looping_is_recursion.clj
@@ -1,22 +1,47 @@
 (ns looping-is-recursion)
 
-(defn power [base exp]
-  ":(")
+((defn power [base exp]
+   (let [helper (fn [base exp real-base]
+                 (if (= 1 exp)
+                   base
+                   (recur (* base real-base) (dec exp) real-base)))]
+    ( if (= 0 exp) 1 (helper base exp base) ) )) 2 0)
 
-(defn last-element [a-seq]
-  ":(")
+((defn last-element [a-seq]
+  (let [helper (fn [a-seq]
+                 (if (= 1 (count a-seq))
+                   (first a-seq)
+                   (recur (rest a-seq))))]
+    ( if (empty? a-seq) nil (helper a-seq) ) )) [])
 
-(defn seq= [seq1 seq2]
-  ":(")
+((defn seq= [seq1 seq2]
+  (let [helper (fn [seq1 seq2]
+                  (if (and (empty? seq1) (empty? seq2)) true
+                    (if (and (= (count seq1) (count seq2)) (= (first seq1) (first seq2)))
+                      (recur (rest seq1) (rest seq2)) false) ))]
+    (helper seq1 seq2)  )) [] [])
 
-(defn find-first-index [pred a-seq]
-  ":(")
+((defn find-first-index [pred a-seq]
+   (loop [predi pred lista a-seq index 0]
+    (cond
+      (empty? lista) nil
+      (predi (first lista)) index
+      :else (recur predi (rest lista) (inc index))))) pos? [-2])
 
-(defn avg [a-seq]
-  -1)
 
-(defn parity [a-seq]
-  ":(")
+((defn avg [a-seq]
+  (if (empty? a-seq) 0 (/ (apply + a-seq) (count a-seq)))) [1 2])
+
+
+(defn lisaa-frekvenssi [frekvenssisetti item]
+  (if (= (get frekvenssisetti item) nil) (assoc frekvenssisetti item 1) (assoc frekvenssisetti item (+ 1 (get frekvenssisetti item)))) )
+
+
+((defn parity [a-seq]
+  (loop [lista a-seq frekvenssisetti {}]
+    (cond
+      (empty? lista) (keys (filter #(even? (mod second 2)) frekvenssisetti))
+      :else (recur (rest lista) (lisaa-frekvenssi frekvenssisetti (first lista)) ) ))) [1 2 2])
 
 (defn fast-fibo [n]
   ":(")
